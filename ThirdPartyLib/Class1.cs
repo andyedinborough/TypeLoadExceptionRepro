a3693f2fd04c25eabc56587d871c3f9c697514b0
Andy Edinborough <andy@edinborough.org>
2020-04-07 15:04:09 -0500

    Initial commit

---
+++ ThirdPartyLib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Razor.Language;
+
+namespace ThirdPartyLib
+{
+    public class Class1
+    {
+        public class CustomHelperResult : HelperResult
+        {
+            public CustomHelperResult(Func<TextWriter, Task> asyncAction) : base(asyncAction)
+            {
+            }
+        }
+
+        private class VirtualRazorProject : RazorProject
+        {
+            public override IEnumerable<RazorProjectItem> EnumerateItems(string basePath) => null;
+
+            public override RazorProjectItem GetItem(string path) => null;
+        }
+    }
+}
